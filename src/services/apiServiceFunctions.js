2a53d88640fb95d6a50dd4ff12bae8cd1e29231b
Adam <adam.john.campbell.murray@gmail.com>
2016-10-19 16:50:50 +0100

    Implement api service functions

--- src/services/apiServiceFunctions.js
+++ src/services/apiServiceFunctions.js
@@ -10,7 +10,7 @@ export {
 }
 
 /* ====================================== */
-/*              Functions
+/*              Public Functions
 /* ====================================== */
 
 /**
@@ -50,28 +50,201 @@ function getAuthedRequest(path, params, apiKey) {
  * 
  * @param { Object|Array|String } state
  * @param { String } type
+ * 
+ * @returns { Object }
  */
 function formatApiData(state, type) {
   switch (type) {
     case 'SEARCH_MOVIE_RESULTS':
-      // return formatMovieSearchResults(state);
+      return formatMovieSearchResults(state);
     
-    case 'SEARCH_TV_RESULTS':
-      // return formatTvSearchResults(state);
+    case 'SEARCH_TVSHOW_RESULTS':
+      return formatTvShowSearchResults(state);
     
     case 'SEARCH_PEOPLE_RESULTS':
-      // return formatActorSearchResults(state);
+      return formatPeopleSearchResults(state);
 
     case 'MOVIE_GENRES':
-      // return formatMovieGenres(state);
+      return formatMovieGenres(state);
 
     case 'TV_GENRES':
-      // return formatTvGenres(state);
+      return formatTvShowGenres(state);
 
     case 'CONFIGURATION':
-      // return formatConfiguration(state);
+      return formatConfiguration(state);
       
     default:
       return state;
   }
+}
+
+/* ====================================== */
+/*              Private Functions
+/* ====================================== */
+
+/**
+ * Format movie search results
+ * 
+ * @private
+ * 
+ * @description
+ * Takes the API data and formats it. Movie 'results' come
+ * back from the API as an array. This is converted here to
+ * a dictionary where the keys are the movie IDs and the values
+ * are the movie data.
+ * 
+ * @param { Object } data - data returned from API
+ * 
+ * @returns { Object }
+ */
+function formatMovieSearchResults(data) {
+  let resultsObj = {};
+  resultsObj.page = data.page;
+  resultsObj.totalPages = data.total_pages;
+  resultsObj.totalResults = data.total_results;
+
+  let moviesById = {};
+
+  _.forEach(data.results, r => {
+    moviesById = addToMovies(moviesById, r);
+  });
+
+  resultsObj.moviesById = moviesById;
+
+  return resultsObj;
+
+  function addToMovies(obj, movie) {
+    let newObj = obj;
+    newObj[movie.id] = movie;
+    return newObj;
+  }
+}
+
+/**
+ * Format tv show search results
+ * 
+ * @private
+ * 
+ * @description
+ * Takes the API data and formats it. TV show 'results' come
+ * back from the API as an array. This is converted here to
+ * a dictionary where the keys are the tv show IDs and the values
+ * are the tv show data.
+ * 
+ * @param { Object } data - data returned from API
+ * 
+ * @returns { Object }
+ */
+function formatTvShowSearchResults(data) {
+  let resultsObj = {};
+  resultsObj.page = data.page;
+  resultsObj.totalPages = data.total_pages;
+  resultsObj.totalResults = data.total_results;
+
+  let tvById = {};
+
+  _.forEach(data.results, r => {
+    tvById = addToTv(tvById, r);
+  });
+
+  resultsObj.tvById = tvById;
+
+  return resultsObj;
+
+  function addToTv(obj, tv) {
+    let newObj = obj;
+    newObj[tv.id] = tv;
+    return newObj;
+  }
+}
+
+/**
+ * Format people search results
+ * 
+ * @private
+ * 
+ * @description
+ * Takes the API data and formats it. People 'results' come
+ * back from the API as an array. This is converted here to
+ * a dictionary where the keys are the people IDs and the values
+ * are the people data.
+ * 
+ * @param { Object } data - data returned from API
+ * 
+ * @returns { Object }
+ */
+function formatPeopleSearchResults(data) {
+  let resultsObj = {};
+  resultsObj.page = data.page;
+  resultsObj.totalPages = data.total_pages;
+  resultsObj.totalResults = data.total_results;
+
+  let peopleById = {};
+
+  _.forEach(data.results, r => {
+    peopleById = addToPeople(peopleById, r);
+  });
+
+  resultsObj.peopleById = peopleById;
+
+  return resultsObj;
+
+  function addTopeople(obj, person) {
+    let newObj = obj;
+    newObj[person.id] = person;
+    return newObj;
+  }
+}
+
+/**
+ * Format movie genres
+ * 
+ * @private
+ * 
+ * @param { Object } data - data returned from API
+ * 
+ * @returns { Object }
+ */
+function formatMovieGenres(data) {
+  let movieGenres = data.genres;
+  let movieGenresById = {};
+
+  _.forEach(movieGenres, (genre) => {
+    movieGenresById[genre.id] = genre.name;
+  });
+
+  return movieGenresById
+}
+
+/**
+ * Format TV show genres
+ * 
+ * @private
+ * 
+ * @param { Object } data - data returned from API
+ * 
+ * @returns { Object }
+ */
+function formatTvShowGenres(data) {
+  let tvGenres = data.genres;
+  let tvGenresById = {};
+
+  _.forEach(tvGenres, (genre) => {
+    tvGenresById[genre.id] = genre.name;
+  });
+
+  return tvGenresById
+}
+
+/**
+ * Format configuration data
+ * 
+ * @private
+ * 
+ * @param { Object } data - data returned from API
+ * 
+ * @returns { Object }
+ */
+function formatConfiguration(data) {
+  return data;
 }
