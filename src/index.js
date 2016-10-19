3458b04fa91873d661a1fab42d56c2c683de7670
Adam <adam.john.campbell.murray@gmail.com>
2016-10-19 16:01:20 +0100

    Implement base index.js file

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App/App';
+import './index.css';
+import './reset.css';
+
+// TODO: uncomment if including service worker in future
+// Register service worker
+// var reg;
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('sw.js').then(() => {
+//       return navigator.serviceWorker.ready;
+//     })
+//     .then((serviceWorkerRegistration) => {
+//       reg = serviceWorkerRegistration;
+//       // console.log('Service Worker is ready :', reg);
+//     })
+//     .catch((error) => {
+//       // console.log('Service Worker Error :', error);
+//     });
+// }
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
